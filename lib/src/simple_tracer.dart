8418fd3e67994f8e9996826590d8af868c7edb8b
Robertas Skiauteris <Shalomka@users.noreply.github.com>
2025-04-16 13:54:43 +0300

    Refactor SimpleTracer class to include constructor and sendTraceData method for Honeycomb integration

--- lib/src/simple_tracer.dart
+++ lib/src/simple_tracer.dart
@@ -1,7 +1,53 @@
+import 'package:dio/dio.dart';
+import 'package:simple_tracer/src/trace_model.dart';
+
 /// {@template simple_tracer}
 /// A Very Good Project created by Very Good CLI.
 /// {@endtemplate}
 class SimpleTracer {
-  /// {@macro simple_tracer}
-  const SimpleTracer();
+  final Dio _dio;
+  final String _serviceName;
+  final String _endpoint;
+  final String _apiKey;
+  const SimpleTracer._(
+    this._dio,
+    this._serviceName,
+    this._endpoint,
+    this._apiKey,
+  );
+
+  /// Creates a new instance of [SimpleTracer] for
+  /// honeycomb.io
+  SimpleTracer.honeycomb({
+    required String serviceName,
+    required String apiKey,
+  }) : this._(
+          Dio(),
+          serviceName,
+          'https://api.honeycomb.io',
+          apiKey,
+        );
+
+  /// Sends trace data to the server.
+  Future<void> sendTraceData(Trace trace) async {
+    final data = trace.encodedData;
+    try {
+      final response = await _dio.post<dynamic>(
+        '$_endpoint/v1/trace',
+        data: data,
+        options: Options(
+          headers: {
+            'X-Honeycomb-Team': _apiKey,
+            'Content-Type': 'application/x-protobuf',
+            'User-Agent': 'simple_tracer/1.0',
+          },
+        ),
+      );
+      if (response.statusCode != 200) {
+        throw Exception('Failed to send trace data: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Failed to send trace data: $e');
+    }
+  }
 }
